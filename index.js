851b59789e9dbcfaf79c1378a2f9fdd9e87a20b6
Jacob Manley <jbmanley45@gmail.com>
2022-03-11 10:17:56 -0800

    commit tree f26b58479c990cae7bc7cc6ed3f903419265dba7

--- index.js
+++ index.js
@@ -11,6 +11,48 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(sentence => { 
+    const words = sentence.split(' ')
+    return words.map(word => { 
+      return word[0].toUpperCase() + word.slice(1)
+     }).join(' ')
+  } )
+ }
+
+    
+
+  //  )
+//}
+
+
+//Action plan:
+//1. Select the first letter of each word
+//2. Upper case each selected letter
+//3. Use the map method on EACH of the arrays that gets produced after x.split
+//TIP:
+//First set x.split to a variable IN the argument of the first map then i can iterate inside of an iterator
+//map returning map returning map. 
+
+
+
+
+
+
+
+
+
+
+//	const titleCased = () => {tutorials.map(x => charAt(0) console.log(x.split(' ')))} 
+//const titleCased = () => {
+//return tutorials.map(x =>
+//console.log(x.split(' ')  
+  //)
+  //}
+  //const titleCased = () => {tutorials.map(x => console.log(a))
+  //  let a = x.split(' ')
+  //} 
+  //const titleCased = () => {
+  //  return tutorials.map(x =>  z= x.split(' ') + x.charAt(0) + x.toUpperCase)}
+     
